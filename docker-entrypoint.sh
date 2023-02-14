d2bbcc1a60f49d53861b28765c61b2ff9af94eda
Balázs Kóta <balazskota@Balazss-MacBook-Pro.local>
2023-02-14 14:47:57 +0100

    Config fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Starting container..."
+echo "Current user: $(whoami)"
+echo "Current user id: $(id -u $(whoami))"
+cd ~
+echo $PWD
+ls -lsa
+
 set -e
 
 exec /usr/bin/python3 /usr/bin/mopidy --quiet --config /usr/share/mopidy/conf.d:/etc/mopidy/mopidy.conf
